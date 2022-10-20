5eb938015aa2c266e4e48be244943669914d7dca
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-10-20 18:09:53 +0100

    worldcoin verification on register and createBounty

--- contracts/r3ktify.sol
+++ contracts/r3ktify.sol
@@ -3,15 +3,30 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import {ByteHasher} from "./helpers/ByteHasher.sol";
+import {IWorldID} from "./interfaces/IWorldID.sol";
 
 contract R3ktify is AccessControl {
+    using ByteHasher for bytes;
+    using Counters for Counters.Counter;
+    Counters.Counter private _bountyId;
+
     // roles
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant PROJECT_ROLE = keccak256("PROJECT_ROLE");
     bytes32 public constant R3KTIFIER_ROLE = keccak256("R3KTIFIER_ROLE");
     bytes32 public constant PERMANENT_BAN_ROLE =
         keccak256("PERMANENT_BAN_ROLE");
     bytes32 public constant TEMPORARY_BAN_ROLE =
         keccak256("TEMPORARY_BAN_ROLE");
+
+    // account types
+    bytes32 public constant PROJECT = keccak256("PROJECT");
+    bytes32 public constant R3KTIFIER = keccak256("R3KTIFIER");
+
+    // immutables
+    IWorldID internal immutable worldId;
 
     // temporary ban time
     uint256 public constant TEMPORARY_BAN_TIME = 24 hours;
@@ -50,12 +65,109 @@ contract R3ktify is AccessControl {
     // mappings
     mapping(address => mapping(uint256 => ProjectBounty)) public bounties;
     mapping(uint256 => Report[]) public reports;
+    mapping(address => bool) public registeredProjects;
+    mapping(address => bool) public registeredR3ktifiers;
+
+    // WorldID group IDs
+    uint256 internal immutable projectGroupId = 1;
+    uint256 internal immutable r3ktifierGroupId = 2;
+
+    // errors
+    error alreadyRegistered();
+    error notAProject();
+
+    constructor(IWorldID _worldID) {
+        // set contract as admin for AccessControl
+        _setupRole(ADMIN_ROLE, address(this));
+        worldId = _worldID;
+    }
 
     // TODO: Implement register function, should accept accountType (project, r3ktifier)
     // TODO: and assign respective role to the account
 
+    function register(
+        bytes32 accountType,
+        address input,
+        uint256 root,
+        uint256 nullifierHash,
+        uint256[8] calldata proof
+    ) public {
+        // require accountType is one of the accepted types
+        require(
+            accountType == PROJECT || accountType == R3KTIFIER,
+            "Unknown account type"
+        );
+
+        // check if user is already registered, revert if true.
+        if (
+            registeredProjects[address(msg.sender)] ||
+            registeredR3ktifiers[address(msg.sender)]
+        ) revert alreadyRegistered();
+
+        if (accountType == PROJECT) {
+            // verify user
+            worldId.verifyProof(
+                root,
+                projectGroupId,
+                abi.encodePacked(input).hashToField(),
+                nullifierHash,
+                abi.encodePacked(address(this)).hashToField(),
+                proof
+            );
+
+            // add the user to the registeredProjects and set role
+            grantRole(PROJECT_ROLE, address(msg.sender));
+            registeredProjects[address(msg.sender)] = true;
+        } else {
+            // check if user is already registered
+            if (registeredR3ktifiers[address(msg.sender)])
+                revert alreadyRegistered();
+
+            // verify user
+            worldId.verifyProof(
+                root,
+                r3ktifierGroupId,
+                abi.encodePacked(input).hashToField(),
+                nullifierHash,
+                abi.encodePacked(address(this)).hashToField(),
+                proof
+            );
+
+            // add the user to the registeredR3ktifiers and set role
+            grantRole(R3KTIFIER_ROLE, address(msg.sender));
+            registeredR3ktifiers[address(msg.sender)] = true;
+        }
+    }
+
     // TODO: Implement createBounty function (only PROJECT_ROLE)
     //? args = rewardBreakdown and projectURI
+    function createBounty(
+        string memory _projectURI,
+        uint256[5] calldata _rewardBreakdown
+    ) public {
+        require(bytes(_projectURI).length != 0, "Project URI needed");
+        require(_rewardBreakdown.length == 6, "6 reward values needed");
+
+        // revert is msg.sender is not a project
+        if (!hasRole(PROJECT_ROLE, address(msg.sender))) revert notAProject();
+
+        // set Project bounty values
+        uint256 currentBountyId = _bountyId.current();
+        ProjectBounty memory _projectBounty = bounties[address(msg.sender)][
+            currentBountyId
+        ];
+
+        // fill in values
+        _projectBounty.bountyId = currentBountyId;
+        _projectBounty.projectURI = _projectURI;
+        _projectBounty.rewardBreakdown = _rewardBreakdown;
+
+        // write bounty to storage
+        bounties[address(msg.sender)][currentBountyId] = _projectBounty;
+
+        // increment _bountyId
+        _bountyId.increment();
+    }
 
     // TODO: Implement submitReport function (only R3KTIFIER_ROLE).
     //? args = reportUri, r3ktifier address, and severity level
