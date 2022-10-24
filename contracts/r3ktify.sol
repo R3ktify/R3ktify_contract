7bfce20b86f7f766557dd5fa709f65ded546aa94
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-10-24 20:37:30 +0100

    submit report, validate report, reward r3ktifier and get all bounties for an address.

--- contracts/r3ktify.sol
+++ contracts/r3ktify.sol
@@ -11,6 +11,7 @@ contract R3ktify is AccessControl {
     using ByteHasher for bytes;
     using Counters for Counters.Counter;
     Counters.Counter private _bountyId;
+    Counters.Counter private _reportId;
 
     // roles
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
@@ -46,15 +47,9 @@ contract R3ktify is AccessControl {
     }
 
     // Sturcts
-    struct ProjectBounty {
-        uint256 bountyId;
-        uint256 submissionCount;
-        uint256[5] rewardBreakdown;
-        string projectURI;
-    }
-
     struct Report {
         uint256 reportId;
+        uint256 rewardAmount;
         string reportUri;
         bool valid;
         address r3ktifier;
@@ -62,11 +57,29 @@ contract R3ktify is AccessControl {
         RewardStatus status;
     }
 
+    struct ProjectBounty {
+        uint256 bountyId;
+        uint256 submissionCount;
+        uint256[5] rewardBreakdown;
+        string projectURI;
+    }
+
     // mappings
-    mapping(address => mapping(uint256 => ProjectBounty)) public bounties;
+    mapping(address => ProjectBounty[]) public bounties;
     mapping(uint256 => Report[]) public reports;
     mapping(address => bool) public registeredProjects;
     mapping(address => bool) public registeredR3ktifiers;
+
+    // events
+    event RewardedR3ktifier(
+        uint256 bountyId,
+        uint256 reportId,
+        address r3ktifier,
+        address project
+    );
+
+    // array
+    address[] public projects;
 
     // WorldID group IDs
     uint256 internal immutable projectGroupId = 1;
@@ -75,6 +88,20 @@ contract R3ktify is AccessControl {
     // errors
     error alreadyRegistered();
     error notAProject();
+    error notAR3ktifier();
+
+    // modifiers
+    modifier onlyProjectRole(address account) {
+        // revert if msg.sender is not a Project
+        if (!hasRole(PROJECT_ROLE, account)) revert notAProject();
+        _;
+    }
+
+    modifier onlyR3ktifierRole(address account) {
+        // revert if msg.sender is not a R3ktifier
+        if (!hasRole(R3KTIFIER, account)) revert notAR3ktifier();
+        _;
+    }
 
     constructor(IWorldID _worldID) {
         // set contract as admin for AccessControl
@@ -118,6 +145,9 @@ contract R3ktify is AccessControl {
             // add the user to the registeredProjects and set role
             grantRole(PROJECT_ROLE, address(msg.sender));
             registeredProjects[address(msg.sender)] = true;
+
+            // add msg.sender to projects array
+            projects.push(msg.sender);
         } else {
             // check if user is already registered
             if (registeredR3ktifiers[address(msg.sender)])
@@ -144,26 +174,24 @@ contract R3ktify is AccessControl {
     function createBounty(
         string memory _projectURI,
         uint256[5] calldata _rewardBreakdown
-    ) public {
+    ) public onlyProjectRole(address(msg.sender)) {
+        // revert if uri = empty string and rewardBreakdown has 6 values
         require(bytes(_projectURI).length != 0, "Project URI needed");
         require(_rewardBreakdown.length == 6, "6 reward values needed");
 
-        // revert is msg.sender is not a project
-        if (!hasRole(PROJECT_ROLE, address(msg.sender))) revert notAProject();
-
         // set Project bounty values
         uint256 currentBountyId = _bountyId.current();
-        ProjectBounty memory _projectBounty = bounties[address(msg.sender)][
-            currentBountyId
-        ];
+        ProjectBounty memory _projectBounty = ProjectBounty({
+            bountyId: currentBountyId,
+            submissionCount: 0,
+            rewardBreakdown: _rewardBreakdown,
+            projectURI: _projectURI
+        });
 
-        // fill in values
-        _projectBounty.bountyId = currentBountyId;
-        _projectBounty.projectURI = _projectURI;
-        _projectBounty.rewardBreakdown = _rewardBreakdown;
+        // push to ProjectBounty array in mapping
 
         // write bounty to storage
-        bounties[address(msg.sender)][currentBountyId] = _projectBounty;
+        bounties[address(msg.sender)].push(_projectBounty);
 
         // increment _bountyId
         _bountyId.increment();
@@ -172,17 +200,125 @@ contract R3ktify is AccessControl {
     // TODO: Implement submitReport function (only R3KTIFIER_ROLE).
     //? args = reportUri, r3ktifier address, and severity level
 
+    function submitReport(
+        uint256 bountyId,
+        address projectAddress,
+        string memory _reportURI,
+        uint8 _severityLevel
+    ) public onlyR3ktifierRole(address(msg.sender)) {
+        // revert if uri = empty string, _severityLevel out of Severity enum range
+        require(bytes(_reportURI).length != 0, "Project URI needed");
+        require(_severityLevel < 5, "Invalid severity level");
+        require(
+            bounties[projectAddress][bountyId].bountyId == bountyId,
+            "Submitting to wrong project"
+        );
+
+        // increment number of submissions for projectBounty
+        bounties[projectAddress][bountyId].submissionCount++;
+
+        //  fill in report values
+        Report memory _report = Report({
+            reportId: _reportId.current(),
+            rewardAmount: 0,
+            reportUri: _reportURI,
+            valid: false,
+            r3ktifier: address(msg.sender),
+            level: Severity(_severityLevel),
+            status: RewardStatus.notRewarded
+        });
+
+        //  store in mapping in projectBounty
+        reports[_reportId.current()].push(_report);
+
+        // increment global report count
+        _reportId.increment();
+    }
+
     // TODO: Implement validate report function (only PROJECT_ROLE),
     // TODO: can update valid and severity fields.
     //? args = valid bool and severity level
+    function validateReport(
+        uint256 bountyId,
+        uint256 reportId,
+        string memory _reportURI,
+        bool _valid,
+        uint8 _severityLevel
+    ) public onlyProjectRole(address(msg.sender)) {
+        // fetch reports data
+        Report storage _tempReport = reports[bountyId][reportId];
+
+        require(
+            keccak256(abi.encodePacked(_tempReport.reportUri)) ==
+                keccak256(abi.encodePacked(_reportURI)),
+            "Wrong report data"
+        );
+        require(_severityLevel < 5, "Invalid severity level");
+
+        // update report data
+        _tempReport.valid = _valid;
+        _tempReport.level = Severity(_severityLevel);
+
+        // set report to new copy with updated values
+        reports[bountyId][reportId] = _tempReport;
+    }
 
     // TODO: Implement rewardR3ktifier function (only PROJECT_ROLE),
     // TODO: recieve reward and reportID. Send reward to r3ktifier address and set rewardStatus to rewarded
     //? args = reportID (reward would be gotten via msg.value)
+    function rewardR3ktifier(uint256 bountyId, uint256 reportId)
+        public
+        payable
+        onlyProjectRole(address(msg.sender))
+    {
+        // fetch reports data
+        Report memory _tempReport = reports[bountyId][reportId];
+
+        require(_tempReport.valid, "Report marked as invalid");
+        require(
+            !hasRole(TEMPORARY_BAN_ROLE, _tempReport.r3ktifier) ||
+                !hasRole(PERMANENT_BAN_ROLE, _tempReport.r3ktifier),
+            "R3ktifier temporarily/permanantly banned"
+        );
+        require(
+            msg.value >=
+                bounties[address(msg.sender)][bountyId].rewardBreakdown[
+                    uint8(_tempReport.level)
+                ],
+            "Wrong reward for severity level"
+        );
+
+        // change report reward status
+        _tempReport.rewardAmount = msg.value;
+        _tempReport.status = RewardStatus.rewarded;
+
+        // send reward to r3ktifier
+        (bool sent, ) = _tempReport.r3ktifier.call{value: msg.value}("");
+        require(sent, "Failed to reward r3ktifier");
+
+        emit RewardedR3ktifier(
+            bountyId,
+            reportId,
+            _tempReport.r3ktifier,
+            address(msg.sender)
+        );
+    }
 
     // TODO: Impplement getAllBounties function
+    function getAllBountiesForAddress(address _projectBounty)
+        public
+        view
+        returns (ProjectBounty[] memory)
+    {
+        return bounties[_projectBounty];
+    }
+
+    function getAllProjects() public view returns (address[] memory) {
+        return projects;
+    }
 
     // TODO: Implement getReports function, (only PROJECT_ROLE),
+    function getReports() public onlyProjectRole(address(msg.sender)) {}
     // TODO: and it should filter reports based on caller address
 
     //! CONTROL FUNCTION
