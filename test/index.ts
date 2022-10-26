87825b778f391a0db3df3d54655720c753b4ba1f
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-10-26 12:25:47 +0100

    tests of smartcontract endpoints

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import moment from "moment";
 // eslint-disable-next-line node/no-missing-import
 import { R3ktify } from "../typechain-types/contracts/r3ktify.sol/R3ktify";
 
@@ -13,6 +14,17 @@ const R3KTIFIER_ROLE = ethers.utils.solidityKeccak256(
   ["string"],
   ["R3KTIFIER_ROLE"]
 );
+
+function convertInput(date: string) {
+  const splitDate: any = date.split(" ");
+  const value: number = parseInt(splitDate[0]);
+  const interval: any = splitDate[1];
+
+  const epoch = moment(new Date()).add(value, interval).toDate();
+  const _epoch = moment(epoch).unix();
+
+  return _epoch;
+}
 
 describe("r3ktifier", function () {
   it("Should return the new greeting once it's changed", async function () {
@@ -48,7 +60,15 @@ describe("r3ktifier", function () {
   it("Should create a bounty", async function () {
     const [, project] = await ethers.getSigners();
 
-    await r3ktify.connect(project).createBounty("aaa", [1, 2, 3, 4, 5]);
+    await r3ktify
+      .connect(project)
+      .createBounty("aaa", [
+        ethers.utils.parseUnits("1", "ether"),
+        ethers.utils.parseUnits("2", "ether"),
+        ethers.utils.parseUnits("3", "ether"),
+        ethers.utils.parseUnits("4", "ether"),
+        ethers.utils.parseUnits("5", "ether"),
+      ]);
   });
 
   it("Should allow r3ktifier to submit a report", async function () {
@@ -64,12 +84,84 @@ describe("r3ktifier", function () {
     ).to.be.revertedWith("Not a R3KTIFIER");
   });
 
-  // it("Should check role", async function () {
-  //   const [, project, r3ktifier] = await ethers.getSigners();
-  //   const check = await r3ktify.hasRole(PROJECT_ROLE, project.address);
-  //   const role = await r3ktify.PROJECT_ROLE();
-  //   console.log("Check: ", check);
-  //   console.log("Role: ", role);
-  //   console.log(PROJECT_ROLE === role);
-  // });
+  it("Should allow project to validate a report", async function () {
+    const [, project] = await ethers.getSigners();
+
+    await r3ktify.connect(project).validateReport(0, 0, "bbb", true, 3);
+  });
+
+  it("Should get report data", async function () {
+    const report = await r3ktify.reports(0, 0);
+
+    expect(report.level).to.be.equal(3);
+  });
+
+  it("Should allow rewarding of r3ktifier", async function () {
+    const [, project] = await ethers.getSigners();
+    const reward: any = ethers.utils.parseUnits("4", "ether");
+    const projectBalance = Number(
+      ethers.utils.formatEther(await project.getBalance())
+    );
+
+    await r3ktify
+      .connect(project)
+      .rewardR3ktifier(project.address, 0, 0, { value: reward });
+
+    expect(projectBalance - reward).to.be.lessThan(projectBalance);
+  });
+
+  it("All get endpoints", async function () {
+    const [, project] = await ethers.getSigners();
+    const bounties = await r3ktify.getAllBountiesForAddress(project.address);
+    const allProjects = await r3ktify.getAllProjects();
+    const reports = await r3ktify.connect(project).getReports(0);
+
+    console.log(`Bounties for ${project.address}: `, bounties);
+    console.log("All projects: ", allProjects);
+    console.log("All reports: ", reports);
+  });
+});
+
+describe("r3ktifier bans", function () {
+  it("Should ban r3ktifier and prevent submission", async function () {
+    const [operator, project, r3ktifier] = await ethers.getSigners();
+    const banTime = await convertInput("25 hours");
+    //  initiate ban
+    const ban = await r3ktify
+      .connect(operator)
+      .temporaryBan(r3ktifier.address, banTime);
+    ban.wait();
+
+    // try to submit a report with banned account
+    await expect(
+      r3ktify.connect(r3ktifier).submitReport(0, project.address, "bbb", 0)
+    ).to.be.revertedWith("Account temporarily banned");
+  });
+
+  it("Should fail to ban a banned account", async function () {
+    const [operator, , r3ktifier] = await ethers.getSigners();
+    const banTime = await convertInput("25 hours");
+    // try to initiate ban
+    await expect(
+      r3ktify.connect(operator).temporaryBan(r3ktifier.address, banTime)
+    ).to.be.revertedWith("Already on temporary ban");
+  });
+
+  it("Should not allow banned (TEMPORARY) account to submit report", async function () {
+    it("Should not allow non-r3ktifier to submit a report", async function () {
+      const [, project] = await ethers.getSigners();
+
+      await expect(
+        r3ktify.connect(project).submitReport(0, project.address, "bbb", 0)
+      ).to.be.revertedWith("Account temporarily banned");
+    });
+  });
+
+  it("Should not allow lifting of ban until cooldown is over", async function () {
+    const [operator, , r3ktifier] = await ethers.getSigners();
+
+    await expect(
+      r3ktify.connect(operator).liftBan(r3ktifier.address)
+    ).to.be.revertedWith("Ban still active");
+  });
 });
